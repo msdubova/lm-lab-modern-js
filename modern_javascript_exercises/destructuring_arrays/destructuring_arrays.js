1c30bca0afa2db9248d7cb5bee228fd43b4e49d9
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-07-13 13:02:11 +0300

    Destructuring_arrays

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -1,9 +1,12 @@
 // Instructions can be found in destructuring_arrays.md
 
-export function sequence(order) {
-  const first = order[0];
-  const second = order[1];
-  const third = order[2];
+// export function sequence(order) {
+//   const first = order[0];
+//   const second = order[1];
+//   const third = order[2];
 
-  return first + ", " + second + ", " + third;
+//   return first + ", " + second + ", " + third;
+// }
+export function sequence(order) {
+  return order.join(', ')
 }
